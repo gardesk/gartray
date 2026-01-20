59fc4ad98f00c6ffbb46c4a7fccb249a0be24f53
mfwolffe <wolffemf@dukes.jmu.edu>
2026-01-20 05:19:29 -0500

    Add tray module skeleton

---
+++ gartray/src/tray/mod.rs
@@ -0,0 +1,8 @@
+//! System tray implementations
+//!
+//! Supports both XEMBED (legacy X11) and StatusNotifierItem (modern D-Bus) protocols.
+
+pub mod xembed;
+// pub mod sni;  // TODO: Sprint 2
+// pub mod icons;
+// pub mod menu;
